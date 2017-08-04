44de91fbbfe653b976e3c1953b219894f9a2d516
no <no>
2017-08-04 11:05:06 +0300

    make code prettier; add [New Search] item; change server domain

--- src/index.js
+++ src/index.js
@@ -1,39 +1,56 @@
-import fetch from "node-fetch";
-import inquirer from "inquirer";
-import cheerio from "cheerio";
-import fs from "q-io/fs";
-import os from "os";
-import path from "path";
-import { exec } from "child-process-promise";
+import fetch from 'node-fetch';
+import inquirer from 'inquirer';
+import cheerio from 'cheerio';
+import fs from 'q-io/fs';
+import os from 'os';
+import path from 'path';
+import { exec } from 'child-process-promise';
+import Player from 'player';
 
-const SERVER_ROOT = "https://myzuka.fm";
-const PLAYER = "vlc";
+const SERVER_ROOT = 'https://myzuka.me';
+const PLAYER = 'vlc';
+const NEW_SEARCH = Symbol('NEW_SEARCH');
 
 const search = async term => {
-    const res = await fetch(`${SERVER_ROOT}/Search/Suggestions?term=${encodeURIComponent(term)}`, {
-        headers: { referer: SERVER_ROOT }
-    });
+    const res = await fetch(
+        `${SERVER_ROOT}/Search/Suggestions?term=${encodeURIComponent(term)}`,
+        {
+            headers: { referer: SERVER_ROOT }
+        }
+    );
     const items = await res.json();
     return items.map(decodeItem);
 };
 
 const askSearchTerm = async () => {
-    return (await inquirer.prompt([ { message: "Search", type: "input", name: "search" } ])).search;
+    return (await inquirer.prompt([
+        { message: 'Search', type: 'input', name: 'search' }
+    ])).search;
 };
 
 const chooseStuff = async suggestions => {
+    const stuffChoices = suggestions.map(item => ({
+        name: `[${item.type}] ${item.value}`,
+        value: item
+    }));
+
+    const newSearchChoice = {
+        name: '[New Search]',
+        value: NEW_SEARCH
+    };
+
     return (await inquirer.prompt([
         {
-            message: "Select the stuff",
-            type: "list",
-            name: "stuff",
-            choices: suggestions.map(item => ({ name: `[${item.type}] ${item.value}`, value: item }))
+            message: 'Select the stuff',
+            type: 'list',
+            name: 'stuff',
+            choices: [...stuffChoices, newSearchChoice]
         }
     ])).stuff;
 };
 
 const decodeItem = item => {
-    const parts = item.url.split("/");
+    const parts = item.url.split('/');
     const type = parts[1];
     return { ...item, type };
 };
@@ -45,16 +62,28 @@ const loadPage = async url => {
 
 const htmlToSongs = html => {
     const $ = cheerio.load(html);
-    const nodes = $(".play [data-url]");
+    const nodes = $('.play [data-url]');
     return nodes
-        .map((i, node) => ({ url: $(node).attr("data-url"), title: $(node).attr("data-title") }))
+        .map((i, node) => ({
+            url: $(node).attr('data-url'),
+            title: $(node).attr('data-title')
+        }))
         .get()
         .map(({ url, title }) => ({ url: `${SERVER_ROOT}${url}`, title }));
 };
 
 const playSongsList = async songs => {
-    const fname = path.join(os.tmpdir(), Math.floor(Math.random() * 1e6).toString(36) + ".m3u");
-    await fs.write(fname, "#EXTM3U\n" + songs.map(({ url, title }) => `#EXTINF:0,${title}\n${url}`).join("\n"));
+    const fname = path.join(
+        os.tmpdir(),
+        Math.floor(Math.random() * 1e6).toString(36) + '.m3u'
+    );
+    await fs.write(
+        fname,
+        '#EXTM3U\n' +
+            songs
+                .map(({ url, title }) => `#EXTINF:0,${title}\n${url}`)
+                .join('\n')
+    );
     await exec(`${PLAYER} ${fname}`);
 };
 
@@ -64,16 +93,19 @@ const getLogo = () => {
 _______
 -_____-
 `;
-}
+};
 
 const displayLogo = () => console.log(getLogo());
 
 const main = async () => {
     displayLogo();
-    const term = await askSearchTerm();
-    const suggestions = await search(term);
-    const item = await chooseStuff(suggestions);
-    const html = await loadPage(item.url);
+    let choice = NEW_SEARCH;
+    while (choice === NEW_SEARCH) {
+        const term = await askSearchTerm();
+        const suggestions = await search(term);
+        choice = await chooseStuff(suggestions);
+    }
+    const html = await loadPage(choice.url);
     const songs = htmlToSongs(html);
     await playSongsList(songs);
 };
