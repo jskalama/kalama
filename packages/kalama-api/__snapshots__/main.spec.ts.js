978554842ba2489717ebfbe7c9593c9378f8c571
no <no>
2017-12-30 20:37:56 +0200

    implement snapshot testing

---
+++ packages/kalama-api/__snapshots__/main.spec.ts.js
@@ -0,0 +1,256 @@
+exports['search should return results 1'] = [
+  {
+    "id": "ASD",
+    "label": "ASD",
+    "value": "ASD",
+    "category": "\u0418\u0441\u043F\u043E\u043B\u043D\u0438\u0442\u0435\u043B\u0438",
+    "image": "https://cs285.myzuka.me/img/68/12799147/33155456.jpg",
+    "url": "https://myzuka.me/Artist/142641/Asd",
+    "songcount": "64",
+    "itemType": 0
+  },
+  {
+    "id": "Asdek",
+    "label": "Asdek",
+    "value": "Asdek",
+    "category": "\u0418\u0441\u043F\u043E\u043B\u043D\u0438\u0442\u0435\u043B\u0438",
+    "image": "",
+    "url": "https://myzuka.me/Artist/206832/Asdek",
+    "songcount": "6",
+    "itemType": 0
+  }
+]
+
+exports['search should return results 2'] = [
+  {
+    "id": "ASD",
+    "label": "A Skylit Drive - ASD (2015)",
+    "value": "ASD",
+    "category": "\u0410\u043B\u044C\u0431\u043E\u043C\u044B",
+    "image": "https://cs25.imyz.me/img/68/10156057/26476247.jpg",
+    "url": "https://myzuka.me/Album/653002/A-Skylit-Drive-Asd-2015",
+    "songcount": "11",
+    "itemType": 1
+  },
+  {
+    "id": "Sag Mir Wo Die Party Ist!",
+    "label": "ASD - Sag Mir Wo Die Party Ist! (2003)",
+    "value": "Sag Mir Wo Die Party Ist!",
+    "category": "\u0410\u043B\u044C\u0431\u043E\u043C\u044B",
+    "image": "https://cs285.imyz.me/img/68/12799233/33191125.jpg",
+    "url": "https://myzuka.me/Album/802749/Asd-Sag-Mir-Wo-Die-Party-Ist-2003",
+    "songcount": "2",
+    "itemType": 1
+  },
+  {
+    "id": "Hey Du (Nimm Dir Zeit) EP",
+    "label": "ASD - Hey Du (Nimm Dir Zeit) EP (2003)",
+    "value": "Hey Du (Nimm Dir Zeit) EP",
+    "category": "\u0410\u043B\u044C\u0431\u043E\u043C\u044B",
+    "image": "https://cs285.imyz.me/img/68/12799223/33191114.jpg",
+    "url": "https://myzuka.me/Album/802737/Asd-Hey-Du-Nimm-Dir-Zeit-Ep-2003",
+    "songcount": "5",
+    "itemType": 1
+  }
+]
+
+exports['search should return results 3'] = [
+  {
+    "id": "ASD",
+    "label": "Scann-Tec - ASD",
+    "value": "ASD",
+    "category": "\u0422\u0440\u0435\u043A\u0438",
+    "image": "",
+    "url": "https://myzuka.me/Song/2547849/Scann-Tec-Asd",
+    "songcount": "",
+    "itemType": 2
+  },
+  {
+    "id": "Asd",
+    "label": "Vienna - Asd",
+    "value": "Asd",
+    "category": "\u0422\u0440\u0435\u043A\u0438",
+    "image": "",
+    "url": "https://myzuka.me/Song/4493665/Vienna-Asd",
+    "songcount": "",
+    "itemType": 2
+  },
+  {
+    "id": "ASD",
+    "label": "Casa Del Mirto - ASD",
+    "value": "ASD",
+    "category": "\u0422\u0440\u0435\u043A\u0438",
+    "image": "",
+    "url": "https://myzuka.me/Song/8200377/Casa-Del-Mirto-Asd",
+    "songcount": "",
+    "itemType": 2
+  },
+  {
+    "id": "ASD",
+    "label": "Paul Miller - ASD",
+    "value": "ASD",
+    "category": "\u0422\u0440\u0435\u043A\u0438",
+    "image": "",
+    "url": "https://myzuka.me/Song/2291193/Paul-Miller-Asd",
+    "songcount": "",
+    "itemType": 2
+  },
+  {
+    "id": "ASD",
+    "label": "ASD - ASD",
+    "value": "ASD",
+    "category": "\u0422\u0440\u0435\u043A\u0438",
+    "image": "",
+    "url": "https://myzuka.me/Song/7223971/Asd-Asd",
+    "songcount": "",
+    "itemType": 2
+  },
+  {
+    "id": "Nel Blue Dipinto Di Blue",
+    "label": "Domenico Modugno - Nel Blue Dipinto Di Blue",
+    "value": "Nel Blue Dipinto Di Blue",
+    "category": "\u0422\u0435\u043A\u0441\u0442\u044B \u043F\u0435\u0441\u0435\u043D",
+    "image": "",
+    "url": "https://myzuka.me/Song/2126739/Domenico-Modugno-Nel-Blue-Dipinto-Di-Blue",
+    "songcount": "",
+    "itemType": 2
+  },
+  {
+    "id": "Gangbang (Feat. Baba Saad & Bass Sultan Hengzt)",
+    "label": "Bushido - Gangbang (Feat. Baba Saad & Bass Sultan Hengzt)",
+    "value": "Gangbang (Feat. Baba Saad & Bass Sultan Hengzt)",
+    "category": "\u0422\u0435\u043A\u0441\u0442\u044B \u043F\u0435\u0441\u0435\u043D",
+    "image": "",
+    "url": "https://myzuka.me/Song/116736/Bushido-Gangbang-Feat-Baba-Saad-And-Bass-Sultan-Hengzt",
+    "songcount": "",
+    "itemType": 2
+  }
+]
+
+exports['getArtistAlbumsList should get a list of artist\'s albums 1'] = [
+  {
+    "url": "https://myzuka.me/Album/291418/Viva-Hits-Vol-20-Cd-1-2003",
+    "label": "Viva Hits, Vol.20 [CD 1]",
+    "albumCategory": 7,
+    "image": "https://cs9.imyz.me/img/68/3335691/16152697.jpg"
+  },
+  {
+    "url": "https://myzuka.me/Album/802749/Asd-Sag-Mir-Wo-Die-Party-Ist-2003",
+    "label": "Sag Mir Wo Die Party Ist!",
+    "albumCategory": 3,
+    "image": "https://cs285.imyz.me/img/68/12799231/33191121.jpg"
+  },
+  {
+    "url": "https://myzuka.me/Album/802737/Asd-Hey-Du-Nimm-Dir-Zeit-Ep-2003",
+    "label": "Hey Du (Nimm Dir Zeit) EP",
+    "albumCategory": 3,
+    "image": "https://cs285.imyz.me/img/68/12799221/33191110.jpg"
+  },
+  {
+    "url": "https://myzuka.me/Album/802736/Asd-Wer-Hatte-Das-Gedacht-2003",
+    "label": "Wer H\u00E4tte Das Gedacht?",
+    "albumCategory": 2,
+    "image": "https://cs285.imyz.me/img/68/12799166/33191058.jpg"
+  },
+  {
+    "url": "https://myzuka.me/Album/291463/Viva-Hits-Vol-21-Cd-1-2003",
+    "label": "Viva Hits, Vol.21 [CD 1]",
+    "albumCategory": 7,
+    "image": "https://cs9.imyz.me/img/68/3336337/16151894.jpg"
+  },
+  {
+    "url": "https://myzuka.me/Album/639833/Asd-Asd-Comeback-2015",
+    "label": "ASD Comeback",
+    "albumCategory": 8,
+    "image": "https://cs0.imyz.me/img/71/9890979/25867344.jpg"
+  },
+  {
+    "url": "https://myzuka.me/Album/802734/Asd-Blockbasta-Deluxe-Edition-2015",
+    "label": "Blockbasta (Deluxe Edition)",
+    "albumCategory": 2,
+    "image": "https://cs285.imyz.me/img/68/12799193/33191084.jpg"
+  }
+]
+
+exports['getTracksList should get a list of artist\'s albums 1'] = [
+  {
+    "url": "https://myzuka.club/Song/Play/7223937?t=636502657952822294&s=2eea1163828b943413598b997d28fcae",
+    "title": "ASD - Die Partei"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223938?t=636502657952822294&s=34e020742db88566df2a8a8ca9486425",
+    "title": "ASD - Blockbasta"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223939?t=636502657952822294&s=5ad5d749fe60269e3e42da3488c8b625",
+    "title": "ASD - Mittelfinga Hoch"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223940?t=636502657952822294&s=3526947ae4374b0d3e4a61b5da0d089a",
+    "title": "ASD - Deadline (Feat. Max Herre)"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223941?t=636502657952822294&s=f1c1919cc89a49fb3110a7ba197ca868",
+    "title": "ASD - Bruda"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223942?t=636502657952978541&s=a5ea634ff48f3eab1e475696d55350ba",
+    "title": "ASD - \u00DCeberall Is Krieg"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223943?t=636502657952978541&s=5270a54e87ac99f97b8de379bf5273df",
+    "title": "ASD - Tortellini Augen"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223944?t=636502657952978541&s=9c4fc20dec2fc6e7421ba37504ece956",
+    "title": "ASD - Legend\u00E4r / Popul\u00E4r"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223945?t=636502657952978541&s=24de6e81f70ac9d8a3f498cf107f01e9",
+    "title": "ASD - Ausrasta"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223946?t=636502657952978541&s=9a4d87377777ac6d2932a45a3b7f611d",
+    "title": "ASD - Airhorn"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223947?t=636502657953134791&s=4853535dfa9ca73464ed5ea0045816a8",
+    "title": "ASD - Antihaltung"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223948?t=636502657953134791&s=00946591ab9d63f0b97865b6b8f6ab61",
+    "title": "ASD - Mensch Gegen Maschine"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223949?t=636502657953134791&s=764542769b42065c7df273595d88cc1a",
+    "title": "ASD - Non"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223950?t=636502657953134791&s=aaf808beb928ea92cfed70b58bc28205",
+    "title": "ASD - Grosses Finale"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223951?t=636502657953134791&s=4732ec0e14c809d5feb7a0f562f2b0d0",
+    "title": "ASD - Ich Seh Was (Feat. Nena)"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223952?t=636502657953134791&s=3763e86f2e801cd2fead47133d3be000",
+    "title": "ASD - Guck Dir Diese Jungs An"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223953?t=636502657953291639&s=5ae7594a39a81462c3f9867e08e69868",
+    "title": "ASD - Goldk\u00FCste (Feat. Megaloh)"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223954?t=636502657953291639&s=770558b3091779235601b0fd944608de",
+    "title": "ASD - Mittelfinga Hoch (RMX) (Feat. Eko Fresh, Ali As & Curse)"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223955?t=636502657953291639&s=b7926a135cd20df37d65f6728e77710c",
+    "title": "ASD - Rap Tag Team"
+  },
+  {
+    "url": "https://myzuka.club/Song/Play/7223956?t=636502657953291639&s=52df18ef9559e7a04da247768ad9cc7b",
+    "title": "ASD - Hase"
+  }
+]
