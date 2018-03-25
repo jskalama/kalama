e2e7cd76ab439ff1dd14b8cf0354385e9b9b8d51
no <no>
2018-03-25 23:02:44 +0300

    upd

--- packages/kalama-mplayer/src/ducks/search.js
+++ packages/kalama-mplayer/src/ducks/search.js
@@ -1,6 +1,7 @@
 // Actions
-export const ON_QUERY_CHANGE = 'SEARCH/ON_QUERY_CHANGE';
-export const ON_QUERY_RESULT = 'SEARCH/ON_QUERY_RESULT';
+export const ON_QUERY_CHANGE = 'Search/ON_QUERY_CHANGE';
+export const ON_QUERY_RESULT = 'Search/ON_QUERY_RESULT';
+export const ON_SUGGECTION_SELECT = 'Search/ON_SUGGECTION_SELECT';
 
 const defaultState = {
     suggestions: []
@@ -25,6 +26,11 @@ export const OnQueryChange = value => ({
 
 export const OnQueryResult = suggestions => ({
     type: ON_QUERY_RESULT,
+    payload: suggestions
+});
+
+export const OnSuggectionSelect = suggestions => ({
+    type: ON_SUGGECTION_SELECT,
     payload: suggestions
 });
 
